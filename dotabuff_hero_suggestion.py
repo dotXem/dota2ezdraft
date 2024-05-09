75a3fffba1f1594699e4fdc2e8d70194133dc48d
Maxime DE BOIS <maxime.debois@gmail.com>
2024-05-09 18:27:27 +0200

    divine bracket stats

--- dotabuff_hero_suggestion.py
+++ dotabuff_hero_suggestion.py
@@ -68,7 +68,7 @@ def get_data_from_stratz():
     querry = """
     {
         heroStats {
-            heroVsHeroMatchup(heroId: {hero_id}) {
+            heroVsHeroMatchup(heroId: {hero_id}, bracketBasicIds: DIVINE_IMMORTAL) {
                 disadvantage {
                     heroId
                     vs {
@@ -434,7 +434,7 @@ nickname_table = {
 
 
 if __name__ == "__main__":
-    collect_today_disadvantages(file_name="dotabuff_data_7-35d_stratz_9-05")
+    collect_today_disadvantages(file_name="dotabuff_data_7-35d_stratz_9-05_divine")
     # suggest_hero(p1="Anti-Mage", p2=None, p3="Dawnbreaker", p4="Mirana", p5="Witch Doctor", filter_list=xem_list)
     # suggest_hero(p1="Chaos Knight", p2="Necrophos", p3=None, p4="Pudge", p5="Grimstroke", filter_list=xem_list)
     
